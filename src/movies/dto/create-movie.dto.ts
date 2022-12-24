68fd058080cd8260b63c880d6156e8d81faf9f11
Augusto Sousa <lagrange.tesla@gmail.com>
2022-12-24 15:40:53 -0600

    Add movies dto

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,1 +1,26 @@
-export class CreateMovieDto {}
+import {
+  IsDate,
+  IsPositive,
+  IsString,
+  Max,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsString()
+  @MinLength(4)
+  review: string;
+
+  @IsPositive()
+  @Min(1)
+  @Max(5)
+  score: number;
+
+  @IsDate()
+  updated_at?: Date;
+}
