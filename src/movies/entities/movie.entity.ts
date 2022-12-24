8b1aa325b844cc0cae0c7f619621e3f80fde8428
Augusto Sousa <lagrange.tesla@gmail.com>
2022-12-23 19:35:55 -0600

    Add movies entity

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,2 +1,26 @@
-@Enti
-export class Movie {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', {
+    unique: true,
+  })
+  movie_name: string;
+
+  @Column('text')
+  review: string;
+
+  @Column('smallint')
+  score: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
